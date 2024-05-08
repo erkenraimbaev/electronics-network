b3e5048b080d40d1a9c72730da94ece7d146c8cc
erkenraimbaev <erken.orsk.2015@gmail.com>
2024-05-08 12:39:19 +0500

    Добавил кликабельную сслыку на поставщика в админ-панели

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.utils.html import format_html
+from rest_framework.response import Response
 
 from main.models import Product, NetworkLink
 
 
 @admin.register(Product)
-class AdAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):
     """
     Ссылка на товары в админ панели
     """
@@ -14,11 +18,19 @@ class AdAdmin(admin.ModelAdmin):
 
 
 @admin.register(NetworkLink)
-class ReviewAdmin(admin.ModelAdmin):
+class NetworkLinkAdmin(admin.ModelAdmin):
     """
     Вывод в админке звена сети электроники с сслыкой на поставщика
     """
-    list_display = ('title', 'network_level', 'supplier', 'country', 'city', 'debt_to_supplier', 'created_at')
+    list_display = ('title', 'network_level', 'display_supplier', 'country', 'city', 'debt_to_supplier', 'created_at')
+
+    # Кликабельная ссылка на поставщика в админке
+    def display_supplier(self, obj):
+        link = reverse("admin:main_networklink_change", args=[obj.id])
+        return format_html('<a href="{}">{}</a>', link, obj.supplier)
+
+    display_supplier.short_description = "Поставщик"
+
     # фильтр по названию города
     list_filter = ('city',)
     search_fields = ('title',)
